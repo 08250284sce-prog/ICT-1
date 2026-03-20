2b3758842cd347d0665107ba8ee9c72f4afe7a27
Sonam Tobgay <08250284.sce@rub.edu.bt>
2026-03-20 16:26:53 +0530

    class activity

---
+++ Demo/Student ActivityEx.py
@@ -0,0 +1,11 @@
+n = input("enter your name:")
+num1 = float(input("enter your first name"))
+num2 = float(input("enter your second name"))
+addition_result = num1 + num2
+subtraction_result = num1 - num2
+multiplication_result = num1 * num2
+division_result = num1 / num2
+print("Adding your number:", addition_result)
+print("Subtracting your number:",subtraction_result)
+print("Multipling your number:",multiplication_result)
+print("Divising your number:",division_result)
