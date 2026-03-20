107fc7088d1b51727b2f915170cb90f24d2eefbd
Sonam Tobgay <08250284.sce@rub.edu.bt>
2026-03-20 15:10:08 +0530

    Arithmeticoperators demo code

---
+++ Demo/ArithmeticOperatorsEx.py
@@ -0,0 +1,14 @@
+a = 10
+b =5
+addiction_result = a + b
+subtraction_result = a-b
+multiplication_result = a * b
+division_result = a / b
+exponentiation_result = a** b
+modulus_result = a % b
+print(addiction_result)
+print(subtraction_result)
+print(multiplication_result)
+print(division_result)
+print(exponentiation_result)
+print(modulus_result)
