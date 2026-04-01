6aeb82bacc331960c923ad7fd70fbf3b70e87420
Sonam Tobgay <08250284.sce@rub.edu.bt>
2026-04-01 10:25:59 +0530

    Nested demo code

---
+++ Demo/NestedEx.py
@@ -0,0 +1,12 @@
+print()
+age = int(input("enter your age:"))
+if age >=18:
+    registered_voter = input("Are you a registered voter?( True/False)")
+    registered_voter = registered_voter.lower()
+    if registered_voter =="true":
+        print("you are eligible to vote.")
+    else:
+        print("you need to register to vote.") 
+else:
+    print("you are not eligible to vote.")
+    print()       
