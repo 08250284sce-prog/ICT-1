0c2d828e63b8afff8a6dc19d1650c6e9c8815011
Sonam Tobgay <08250284.sce@rub.edu.bt>
2026-04-09 16:44:10 +0530

    Adding list code

---
+++ Demo/list.py
@@ -0,0 +1,23 @@
+print()
+li = ["python programming","python fundamental","python questions"]
+for x in li:
+    print(x)
+print()  
+lenli = len(li) 
+for x in range(lenli): 
+    print(li[x])
+print()
+
+tuple_list = tuple(li)
+for x in tuple_list:
+    print(x)
+print()    
+Set_list = set(li)
+for x in Set_list:
+    print(x)
+print()
+
+
+
+
+
