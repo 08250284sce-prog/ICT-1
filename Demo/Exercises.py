76d2889794ef222bc829d617684c6be2fd52169d
Sonam Tobgay <08250284.sce@rub.edu.bt>
2026-04-22 21:35:58 +0530

    adding exercises code

--- Demo/Exercises.py
+++ Demo/Exercises.py
@@ -1,19 +1,21 @@
 print()
-def calculate_total(m1, m2, m3):
-    m1 = int(input("enter the marks of the subject 1:"))
-    m2 = int(input("enter the marks of the subject 2:"))
-    m3 = int(input("enter the marks of the subject 3:"))
-    return m1, m2, m3
-
 def calculate_total(m1,m2,m3):
     return m1+m2+m3
 
-def cailculate_average(total):
+def calculate_average(total):
     return total/3
 
-def get_result(average):
-    if average >= 50:
-        return "pass"
-    else:
-        return "fail"
+m1 = float(input("enter the marks of the subject 1:"))
+m2 = float(input("enter the marks of the subject 2:"))
+m3 = float(input("enter the marks of the subject 3:"))
+
+total = calculate_total(m1,m2,m3)
+average = calculate_average(total)
+
+print("total: ", total)
+print(f'Average: , {average:.2f}')
+if average >= 50:
+    print( "pass")
+else:
+    print( "fail")
 print()     
