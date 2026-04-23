92f5ade59e879a76a4dcb0a852486179391b5495
Sonam Tobgay <08250284.sce@rub.edu.bt>
2026-04-23 20:16:13 +0530

    adding exercises code

--- Demo/Exercises.py
+++ Demo/Exercises.py
@@ -4,6 +4,11 @@ def calculate_total(m1,m2,m3):
 
 def calculate_average(total):
     return total/3
+def check_even_odd(num):
+    if num % 2==0:
+        return "Even"
+    else:
+        return "Odd"
 
 m1 = float(input("enter the marks of the subject 1:"))
 m2 = float(input("enter the marks of the subject 2:"))
@@ -18,4 +23,6 @@ if average >= 50:
     print( "pass")
 else:
     print( "fail")
+num = int(input("Enter the number to check even /odd: "))  
+print("The number is: ",check_even_odd(num))  
 print()     
